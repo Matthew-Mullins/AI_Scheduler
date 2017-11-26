297dce65e6f075d563ed7bb8ad26f8136ed1215b
Rhys <rhys.soz@gmail.com>
2017-11-26 14:25:58 -0700

    Added better method names to PreferenceTriple, fleshed out eval some

--- Scheduler/src/artificial/intelligence/cpsc/evalCheck.java
+++ Scheduler/src/artificial/intelligence/cpsc/evalCheck.java
@@ -51,8 +51,23 @@ public class evalCheck {
 	//TODO scan through the preferences of the professors, a collection of course/slot/triple combinations
 	//check at each loop head if the Course from the last loop is still the course in question (as every preference
 	//for a course needs to be violated before the penalty is incurred)
+	
+	//ASSUMED: The list of preferences parsed is grouped into courses, in other words, after a new Course value in the triple
+	//is reached in the list, the courses already seen earlier in the list will not occur later, after a break of other courses
+	//or labs
+	//WORKING ON THIS ONE STILL- NOT DONE -RHYS
 	public float preferenceCheck(preferenceTriple[] preferences){
-		return 0;
+		Classes currentClass = null;
+		float penaltyTotal  = 0;
+		float loopPenalty = preferences[0].getPenalty();
+		boolean failedAllFlag = false;
+		for(int i = 0; i< preferences.length; i++){
+			if(currentClass != preferences[0].getClasses()){
+				
+			}
+			
+		}
+		return penaltyTotal;
 	}
 	//TODO scan through the list of proposed pairs, checking if the timeSlots for them in the assign 
 	//are the same, incrementing the penalty if not
--- Scheduler/src/artificial/intelligence/cpsc/preferenceTriple.java
+++ Scheduler/src/artificial/intelligence/cpsc/preferenceTriple.java
@@ -1,18 +1,23 @@
 package artificial.intelligence.cpsc;
 
+//
+//Simple structure to hold a TimeSlot, Course and Penalty if that course is not in
+//the given timeslot. Parser should order these into like courses and create a list of them
+//from the input file
+
 public class preferenceTriple {
 	private final TimeSlot slot;
-	private final Course course;
+	private final Classes courseOrLab;
 	private final float penalty;
 	
-	public preferenceTriple(TimeSlot givenSlot, Course givenCourse, float pen){
+	public preferenceTriple(TimeSlot givenSlot, Classes givenCourseOrLab, float pen){
 		slot = givenSlot;
-		course = givenCourse;
+		courseOrLab = givenCourseOrLab;
 		penalty = pen;
 	}
 	
 	public TimeSlot getTime(){return slot; }
-	public Course getCourse(){return course; }
+	public Classes getClasses(){return courseOrLab; }
 	public float getPenalty(){return penalty; }
 
 }
